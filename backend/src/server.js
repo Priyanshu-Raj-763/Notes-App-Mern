b6a5d19395c3214c95f5c64fc851382410b81e36
Techno Boy <technoboypriyanshu763@gmail.com>
2025-11-11 09:54:09 +0530

    HomePage navbar Notecard fetched notes from backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,7 +1,8 @@
 import express from "express"
+import dotenv from "dotenv"
+import cors from "cors"
 import notesRoutes from "./routes/notesRoutes.js"
 import { connectDb } from "./config/db.js";
-import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
 
 dotenv.config()
@@ -15,6 +16,10 @@ app.get("/", (req, res) => {
 });
 
 //middleware
+app.use(cors({
+    origin:"http://localhost:5173",
+})
+);
 app.use(express.json()) // this middle ware allows you to get access to req.body
 app.use(rateLimiter)
 app.use("/api/notes", notesRoutes)
